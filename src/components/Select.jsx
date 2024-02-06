822679943f1bd8e75136ce4d8cf00c0f8cd9f5d4
karma-A <kkh2002csw@gmail.com>
2024-02-06 22:06:08 +0630

    sign

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -28,7 +28,7 @@ const Select = () => {
                 <div >{title}</div>
                 <div className="link">
                     {link}
-                    <div className="sign">{isActive ? '🔺' : '🔻'}</div>
+                    <div className="sign">{isActive ? '-' : '+'}</div>
                 </div>
 
                 </div>
