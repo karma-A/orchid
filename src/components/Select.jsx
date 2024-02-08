41f55eb62c53b3fa0a99869c0bf566538d967b8f
karma-A <kkh2002csw@gmail.com>
2024-02-08 12:26:59 +0630

    .

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -10,7 +10,7 @@ const Select = () => {
         title: 'Burmese Orchid',
         logo: './logo.png',
         link: 'How to idenify',
-        leftContent: `Official website links end with .org`,
+        leftContent: `Official website links end with .org `,
         trustedWeb: `https://www.burmeseorchid.org/trusted-websites`,
         rightContent: 'Secure websites use HTTPS'
         };
